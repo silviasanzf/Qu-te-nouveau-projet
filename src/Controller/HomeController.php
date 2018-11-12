d05e5b5e75023ecd523bd56a709c26d7292aa675
Silvia Sanz <silviasanz3@yahoo.fr>
2018-11-12 11:14:07 +0100

    index

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder
+ * Date: 12/11/18
+ * Time: 10:30
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home")
+     */
+
+
+
+
+    public function index()
+    {
+
+        return $this->render('/home.html.twig');
+
+
+        #return new Response('Hello WCS');
+    }
+
+}
